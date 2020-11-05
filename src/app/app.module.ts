5d8f702b22afd37dd95010864c586f69813db267
Jonas Karlsson <jk@xdy.se>
2020-11-05 10:19:31 +0100

    Milestone 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,10 @@ import {NgModule} from '@angular/core';
 import {AppComponent} from './app.component';
 import {CrisisListComponent} from './crisis-list/crisis-list.component';
 import {HeroListComponent} from './hero-list/hero-list.component';
-import {RouterModule, Routes} from "@angular/router";
 import {FormsModule} from "@angular/forms";
 import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
-
-const appRoutes:Routes = [
-  {path: 'crisis-center', component: CrisisListComponent},
-  {path: 'heroes', component: HeroListComponent},
-  {path: '', redirectTo: '/heroes', pathMatch: 'full'},
-  {path: '**', component: PageNotFoundComponent}
-];
+import {AppRoutingModule} from './app-routing.module';
+import {RouterModule} from "@angular/router";
 
 @NgModule({
   declarations: [
@@ -23,9 +17,8 @@ const appRoutes:Routes = [
     PageNotFoundComponent
   ],
   imports: [
-    BrowserModule, FormsModule, RouterModule.forRoot(appRoutes, {enableTracing: true})
+    BrowserModule, FormsModule, AppRoutingModule, RouterModule
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule {
